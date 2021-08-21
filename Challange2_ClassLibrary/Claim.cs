abdfc05dd1fd0c9c340a32a098fe90296f938983
Jack McNeil <mcneil.jack.m@gmail.com>
2021-08-21 16:06:06 -0400

    Claims class challange 2

---
+++ Challange2_ClassLibrary/Claim.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange2_ClassLibrary
+{
+    public enum ClaimTypes{ Car, Home, Theft};
+    public class Claim
+    {
+        public int ClaimId { get; set; }
+        public ClaimTypes ClaimType { get; set; }
+        public string Description { get; set; }
+        public double ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid
+        {
+            get
+            {
+                TimeSpan length = DateOfClaim - DateOfIncident;
+            }
+        }
+
+    }
+}
--- Challange2_UnitTest/UnitTest1.cs
+++ Challange2_UnitTest/UnitTest1.cs
@@ -9,6 +9,18 @@ namespace Challange2_UnitTest
         [TestMethod]
         public void TestMethod1()
         {
+            DateTime claim = new DateTime(2021, 08, 21);
+            DateTime incident1 = new DateTime(2021, 08, 1);
+            DateTime incident2 = new DateTime(2021, 07, 1);
+
+            TimeSpan length = claim - incident1;
+            TimeSpan length2 = claim - incident2;
+
+            bool greaterThan30 = length2.TotalDays > 30;
+            Console.WriteLine(length);
+            Console.WriteLine(length2);
+
+            Assert.IsTrue(greaterThan30);
         }
     }
 }
