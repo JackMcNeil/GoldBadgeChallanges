5f6c0f381d4dc296804bcafe605b7546aa1c9f21
Jack McNeil <mcneil.jack.m@gmail.com>
2021-08-22 21:48:32 -0400

    menu for challange 2

---
+++ Challange2_ConsoleApp/UI/ProgramUI.cs
@@ -0,0 +1,15 @@
+﻿using Challange2_ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange2_ConsoleApp.UI
+{
+    public class ProgramUI
+    {
+        private readonly ClaimsRepository _repo = new ClaimsRepository();
+    }
+}
