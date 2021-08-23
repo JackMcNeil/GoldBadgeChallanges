00be70a0dd91636ebe09d719a48febb55b02cdb7
Jack McNeil <mcneil.jack.m@gmail.com>
2021-08-23 14:07:00 -0400

    menu challange 3

---
+++ Challange3_ConsoleApp/UI/ProgramUI.cs
@@ -0,0 +1,30 @@
+﻿using Challange3_ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange3_ConsoleApp.UI
+{
+    public class ProgramUI
+    {
+        private readonly BadgeRepo _repo = new BadgeRepo();
+
+        public void Run()
+        {
+            SeedContent();
+            Menu();
+        }
+
+        public void SeedContent()
+        {
+
+        }
+
+        public void Menu()
+        {
+
+        }
+    }
+}
