ba272d50106329f5d8030f7bed6f25a0c2399cfa
Jack McNeil <mcneil.jack.m@gmail.com>
2021-08-24 13:27:34 -0400

    challange 5 class

---
+++ Challange5_ClassLibrary/Customer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange5_ClassLibrary
+{
+    public enum TypeOfCustomer { Potential, Current, Past}
+
+    public class Customer
+    {
+        Dictionary<TypeOfCustomer, string> emailDictionary = new Dictionary<TypeOfCustomer, string>()
+        {
+            {TypeOfCustomer.Potential, "We currently have the lowest rates on Helicopter Insurance!"},
+            {TypeOfCustomer.Current, "Thank you for your work with us. We appreciate your loyalty. Here's a coupon" },
+            {TypeOfCustomer.Past, "It's been a long time since we've heard from you, we want you back" }
+        };
+ 
+        public Customer() { }
+        public Customer(string firstName, string lastName, TypeOfCustomer customerType)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            CustomerType = customerType;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return FirstName + LastName;
+            }
+        }
+        public TypeOfCustomer CustomerType { get; set; }
+        public string Email
+        {
+            get
+            {
+                return emailDictionary[CustomerType];
+            }
+        }
+    }
+}
---
+++ Challange5_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challange5_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challange5_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bf005ae8-7d77-4fa7-ae8c-b9e758e562fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
