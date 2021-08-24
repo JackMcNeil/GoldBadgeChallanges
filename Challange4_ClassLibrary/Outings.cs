f785995959c7ffc39b75f548a2a7d5fc3afb015f
Jack McNeil <mcneil.jack.m@gmail.com>
2021-08-24 09:33:31 -0400

    challange 4 class

---
+++ Challange4_ClassLibrary/Outings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challange4_ClassLibrary
+{
+    public enum EventTypes { Golf, Bowling, AmusementPark, Concert}
+    public class Outings
+    {
+        public Outings() { }
+        public Outings(EventTypes eventType, int attendees, DateTime date, double totalCost)
+        {
+            Event = eventType;
+            Attendees = attendees;
+            Date = date;
+            TotalCost = totalCost;
+        }
+
+        public EventTypes Event { get; set; }
+        public int Attendees { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalCost { get; set; }
+        public double TotalCostPerPerson
+        {
+            get
+            {
+                return TotalCost / Attendees;
+            }
+        }
+    }
+}
---
+++ Challange4_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challange4_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challange4_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("906ab24a-9eea-43d4-97c3-16b9e53cbeea")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
